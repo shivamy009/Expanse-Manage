8b1b3903ad35805dc899c63ba12f56ae86baf45c
shivamy009 <shivam0342005@gmail.com>
2025-02-18 16:03:29 +0530

    header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -10,7 +10,7 @@ import React from 'react'
 const Header = () => {
      
   return (
-    <div className=' p-5 flex justify-between items-center border shadow-sm'>
+    <div className=' p-3 flex justify-between items-center border shadow-sm bg-blue-500'>
         <Image src={'./logo.svg'} alt='logo' height={100} width={160}/>
         
             <Link href={'/dashboard'}><Button>Get Started</Button></Link>
