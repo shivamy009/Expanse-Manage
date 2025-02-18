4925c7c83ee325328fe961d119d205d0c40ba6d0
shivamy009 <shivam0342005@gmail.com>
2025-02-18 14:47:52 +0530

    hi

--- utils/index.jsx
+++ utils/index.jsx
@@ -5,7 +5,7 @@
 
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-const sql = neon('postgresql://neondb_owner:npg_3kLecivfPY5z@ep-billowing-feather-a5vgqz4r.us-east-2.aws.neon.tech/Expanse-tracker?sslmode=require');
+const sql = neon(' ');
 import * as schema from './schema'
 export const db = drizzle({ client: sql },{schema});
 
